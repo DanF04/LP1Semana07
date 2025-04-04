cbfe5248f509a6992120dca0935af5989405fe03
Daniel <danielmordelfer@gmail.com>
2025-04-04 16:50:28 +0100

    fixed program

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -6,7 +6,32 @@ namespace ColorSpheres
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Color color = new Color(80, 70, 200);
+            Color color2 = new Color(200, 80, 70);
+
+            Sphere sphere1 = new Sphere(color, 5);
+            Sphere sphere2 = new Sphere(color2, 8);
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
+            
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
+            
+            sphere2.Pop();
+            sphere1.Throw();
+            sphere1.Throw();
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            
+            Console.WriteLine($"Sphere1: {sphere1.GetTimesThrown()}");
+            Console.WriteLine($"Sphere2: {sphere2.GetTimesThrown()}");
         }
     }
 }
