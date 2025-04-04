20f7170391ed6b1ed0882f42f9458ff74370a20e
Daniel <danielmordelfer@gmail.com>
2025-04-04 14:21:30 +0100

    New program and another program that has the class player

---
+++ MyRPG/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+{
+
+    public class Player
+    {
+        private int XP
+        private float health
+        public string Name { get; }
+
+        public int XP
+        {
+            get => return xp;
+        }
+
+        public int Level
+        {
+            get => 1 + XP/1000;
+        }
+        public float Health
+        {
+
+            get => return health;
+                
+
+            set => (health < 0) health = 0;
+                
+        }
+
+        public float MaxHealth 
+        {
+            get => 100 + (Level - 1) * 20
+        }
+    }
+}
