627d645b870f90709c4359755e060495437bdd37
Daniel <danielmordelfer@gmail.com>
2025-04-04 16:51:18 +0100

    fixed sphere

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -1,12 +1,25 @@
-﻿using System;
-
-namespace ColorSpheres
+﻿namespace ColorSpheres
 {
-    public class Program
+    public class Sphere
     {
-        private static void Main(string[] args)
+        private readonly Color _color;
+        private float _radius;
+        private int _shot;
+
+        public Sphere(Color color, float radius)
         {
-            Console.WriteLine("Hello LP1!");
+            _color = color;
+            _radius = radius;
+            _shot = 0;
         }
+
+        public void Pop() => _radius = 0;
+
+        public void Throw()
+        {
+            if (_radius > 0) _shot++;
+        }
+
+        public int GetTimesThrown() => _shot;
     }
 }
