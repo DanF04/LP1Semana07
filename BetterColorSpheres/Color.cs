36045f1809972ce0240f089e2b38e881f259012d
Daniel <danielmordelfer@gmail.com>
2025-04-04 16:57:18 +0100

    fixed color class

--- BetterColorSpheres/Color.cs
+++ BetterColorSpheres/Color.cs
@@ -2,11 +2,28 @@
 
 namespace BetterColorSpheres
 {
-    public class Program
+    public class Color
     {
-        private static void Main(string[] args)
+
+        public byte red { get; }
+        public byte green { get; }
+        public byte blue { get; }
+        public byte alpha { get; }
+
+        public Color(byte red, byte green, byte blue) : this(red, green, blue, 255) {}
+        
+        public Color(byte red, byte green, byte blue, byte alpha)
         {
-            Console.WriteLine("Hello LP1!");
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
         }
+
+        public byte GetGrey()
+        {
+            return Byte.Parse(((red+green+blue)/3).ToString());
+        }
+        
     }
 }
