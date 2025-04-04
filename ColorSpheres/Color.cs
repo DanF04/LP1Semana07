770ae555ab7cf7b68ca12d5dae584dab3e1542ce
Daniel <danielmordelfer@gmail.com>
2025-04-04 16:50:54 +0100

    fixed color class

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -2,11 +2,31 @@
 
 namespace ColorSpheres
 {
-    public class Program
+    public class Color
     {
-        private static void Main(string[] args)
+        private readonly byte _red;
+        public byte GetRed() => _red;
+        
+        private readonly byte _green;
+        public byte GetGreen() => _green;
+        
+        private readonly byte _blue;
+        public byte GetBlue() => _blue;
+        
+        private readonly byte _alpha;
+        public byte GetAlpha() => _alpha;
+        
+        public Color(byte red, byte green, byte blue, byte alpha = 255)
         {
-            Console.WriteLine("Hello LP1!");
+            _red = red;
+            _green = green;
+            _blue = blue;
+            _alpha = alpha;
+        }
+
+        public byte GetGrey()
+        {
+            return Byte.Parse(((_red+_green+_blue)/3).ToString());
         }
     }
 }
